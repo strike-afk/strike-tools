cabc50649f140e1ad1bf2d71d1100ad62987a5b2
strike-afk <81771886+strike-afk@users.noreply.github.com>
2021-04-29 17:49:59 +0300

    Add files via upload
    Termux için 22 tool

---
+++ strike-tools.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+
+#github:strike-afk
+#Youtube:STRİKE
+#Codded by/Strike
+import os  
+import sys
+import time
+#STRİKE BURADA ☾★
+
+#BANNER
+print("Yükleniyor..")
+os.system("sleep 3")
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+os.system("clear")
+print("""
+\033[91m
+                   -`
+                  .o+`
+                 `ooo/
+                `+oooo:
+               `+oooooo:
+               -+oooooo+:
+             `/:-:++oooo+:
+            `/++++/+++++++:
+           `/++++++++++++++:
+          `/+++ooooooooooooo/`
+         ./ooosssso++osssssso+`
+        .oossssso-ssoo/ossssss+`
+       -osssssso+++++++:sssssso+`
+      /sss/+\033[94mCodded by/Strike\033[91m+oss.
+     /ossssssss/+ooooooox+ssssooo/-
+   `/ossssso+/++++++++++-:/+osssso+-
+   `+sso+osssssssssssssssxpo+++/+oso:
+  `++:+os+oooooooooooosssxo+sooss`-/+/
+ .\033[94m☾★                                  ☾★
+        \033[94mInstagram:strike.aser
+ ☾★     \033[92mYoutube  :STRİKE\033[94m               ☾★
+        \033[94MGithub   :strike-afk      
+☾★                                      ☾★
+\033[93m[1]\033[94mXerXes                       \033[93m[12]\033[94mT-U-R-K
+\033[93m[2]\033[94mgrabcam                      \033[93m[13]\033[94mCamera-tr
+\033[93m[3]\033[94mipmux                        \033[93m[14]\033[94mFinduser
+\033[93m[4]\033[94mIg-brute                     \033[93m[15]\033[94mTermux_Full_Kurulum
+\033[93m[5]\033[94mAdmin Panel finder           \033[93m[16]\033[94mipolustur
+\033[93m[6]\033[94malgosspammer                 \033[93m[17]\033[94mtorshammer
+\033[93m[7]\033[94mTBomb                        \033[93m[18]\033[94mOsi-ig
+\033[93m[8]\033[94mStrike                       \033[93m[19]\033[94mphishing
+\033[93m[9]\033[94mimpulse                      \033[93m[20]\033[94mTermuxu güncelle
+\033[93m[10]\033[94mtc-kimlik                   \033[93m[21]\033[94msite-ping
+\033[93m[11]\033[94mOSIF                        \033[93m[22]\033[94mHack-Tools
+                 \033[92m[0]Çıkış 
+""")
+strike=input("\033[93mişlem numarasını seçiniz: ")
+
+if strike=="1":
+        os.system("git clone " " https://github.com/CyberXCodder/XerXes")
+if strike=="2":
+        os.system("git clone " " https://github.com/noob-hackers/grabcam")
+
+if strike=="3":
+        os.system("git clone " " https://github.com/Amriez/ipmux")
+if strike=="4":
+        os.system("git clone " " https://github.com/keralahacker/Ig-brute")
+if strike=="5":
+        os.system("git clone " " https://github.com/bdblackhat/admin-panel-finder")
+if strike=="6":
+        os.system("git clone " " https://github.com/algospoines/algosspammer")
+if strike=="7":
+        os.system("git clone " " https://github.com/Hackertrackersj/Tbomb")
+if strike=="8":
+        os.system("git clone " " https://github.com/strike-afk/Strike")
+if strike=="9":
+        os.system("git clone " " https://github.com/LimerBoy/Impulse")
+if strike=="10":
+        os.system("git clone " " https://github.com/ibrahimirdem/tckimlik")
+if strike=="11":
+        os.system("git clone " " https://github.com/CiKu370/OSIF")
+if strike=="12":
+        os.system("git clone " " https://github.com/yamanefkar/T-U-R-K")
+if strike=="13":
+       os.system("git clone " " https://github.com/yamanefkar/Camera-Tr")
+if strike=="14":
+       os.system("git clone " " https://github.com/xHak9x/finduser")
+if strike=="15":
+       os.system("git clone " " https://github.com/M49R0/Termux_Full_Kurulum")
+if strike=="16":
+       os.system("git clone " " https://github.com/saepsh/ipolustur")
+if strike=="17":
+       os.system("git clone " " https://github.com/dotfighter/torshammer")
+if strike=="18":
+       os.system("git clone " " https://github.com/th3unkn0n/osi.ig")
+if strike=="19":
+       os.system("git clone " " https://github.com/xHak9x/SocialPhish")
+if strike=="20":
+       os.system("apt update -y")
+       os.system("apt upgrade -y")
+if strike=="21":
+       os.system("git clone " " https://github.com/strike-afk/site-ping")
+if strike=="22":
+       os.system("git clone " " https://github.com/yamanefkar/Hack-Tools")
+
+else:
+       input("\033[94mdevam etmek için enter'a bas       ")
+if strike=="0":
+       print("\033[94mÇıkış Yapılıyor...")
+       os.system("sleep 3")
+       print()
+       print()
+       print("\033[94mÇıkış Yapıldı√")
+       os.system("sleep 2")
+       quit()
+else:
+       os.system("python strike-Tools.py")
+       print()
+
+
+
